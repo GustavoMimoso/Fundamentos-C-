d0e6e3339d451845645e12eb78c4fe760ee19da9
Gustavo Mimoso <guraincrivel@hotmail.com>
2025-09-25 15:01:36 -0300

    Primerios exemplos de fundamentos

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using ExemploFundamentos.Models;
+
+DateTime dataAtual = DateTime.Now.AddDays(12);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+
+
+// string apresentacao = "Ola, seja bem vindo";
+// int quantidade = 1;
+// double altura = 1.80;
+// decimal preco = 1.80M;
+// bool condicao = true;
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variavel quantidade: "+ quantidade);
+// Console.WriteLine("Valor da variavel altura: "+ altura);
+// Console.WriteLine("Valor da variavel preco: "+ preco);
+// Console.WriteLine("Valor da variavel condicao: "+ condicao);
+// //Pessoa pessoa1 = new Pessoa();
+
+// //pessoa1.Nome = "Gustavo";
+// //pessoa1.Idade = 23;
+// //pessoa1.Apresentar();
